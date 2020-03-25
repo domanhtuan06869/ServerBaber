a9545401f927935f4ae0cd3809ce2ea951605976
TuanDoPt <domanh.tuanpt@gmail.com>
2020-03-25 14:19:25 +0700

    update booked

---
+++ model/guest_booked.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const GuestBooked = new mongoose.Schema({
+    numberPhone: {
+        type: String,
+        trim: true
+    },
+    name: {
+        type: String,
+        trim: true
+    },
+    bookingTime: {
+        type: String,
+        trim: true,
+    },
+    menberCut: {
+        type: String,
+        trim: true,
+    },
+    addressStore: {
+        type: String,
+        trim: true,
+    },
+    idStore: {
+        type: String,
+        trim: true,
+    },
+}, { versionKey: false });
+module.exports = mongoose.model('guestbooked', GuestBooked);
