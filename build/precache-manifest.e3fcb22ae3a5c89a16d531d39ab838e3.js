dd4300bb5d9bc86328b65d126b32072676181c65
Đỗ Mạnh Tuân <46439071+domanhtuan06869@users.noreply.github.com>
2020-05-09 21:59:44 +0700

    update version

---
+++ build/precache-manifest.e3fcb22ae3a5c89a16d531d39ab838e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8ba396ba75130ebab844c5f6532ff3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c515a08aed3a73522d10",
+    "url": "/static/css/2.373d3ec3.chunk.css"
+  },
+  {
+    "revision": "e4ccccc3f0fb4123848b",
+    "url": "/static/css/main.693ddd7c.chunk.css"
+  },
+  {
+    "revision": "c515a08aed3a73522d10",
+    "url": "/static/js/2.cbb83fb8.chunk.js"
+  },
+  {
+    "revision": "e4ccccc3f0fb4123848b",
+    "url": "/static/js/main.04bc1f52.chunk.js"
+  },
+  {
+    "revision": "f39824ec7e4f6722ff55",
+    "url": "/static/js/runtime-main.70814f86.js"
+  },
+  {
+    "revision": "18c8b5793453537e6940bf0cd36aa498",
+    "url": "/static/media/testimonisls.18c8b579.png"
+  }
+]);
