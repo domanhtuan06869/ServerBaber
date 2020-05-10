4a2b9b8c9ed782664c321868b00477df801aff42
Đỗ Mạnh Tuân <46439071+domanhtuan06869@users.noreply.github.com>
2020-05-10 12:46:47 +0700

    update code

--- routes/controller.js
+++ routes/controller.js
@@ -82,4 +82,44 @@ router.delete('/deleteOder',withAuth, function (req, res, next) {
     res.send(deleteOder)
 });
 
+router.get("/searchProduct", (req, res) => {
+    var name = req.query.name;
+    if (!name) {
+        res.send("VUi long khong de trong tên sản phẩm");
+        //nếu có lỗi thì dừng luôn
+        return;
+    }
+    Product.find({ nameProduct: name }).exec((err, docs) => {
+        res.send(docs)
+    })
+})
+
+//Sắp xếp sản phẩm tăng dần
+router.get("/result/asc", (req, res) => {
+    var id = req.query.id;
+    Product.find({ typeProduct: id }).sort({ priceProduct: 1 }).exec((err, docs) => {
+        res.send(docs)
+    })
+})
+
+//Sắp xếp sản phẩm giảm dần
+router.get("/result/dsc", (req, res) => {
+    var id = req.query.id;
+    Product.find({ typeProduct: id }).sort({ priceProduct: -1 }).exec((err, docs) => {
+        res.send(docs)
+    })
+})
+
+router.get("/result", (req, res) => {
+    var id = req.query.id;
+    if (!id) {
+        res.send("VUi long khong de trong id");
+        //nếu có lỗi thì dừng luôn
+        return;
+    }
+    Product.find({ typeProduct: id }).exec((err, docs) => {
+        res.send(docs)
+    })
+})
+
 module.exports = router;
